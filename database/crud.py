45d9613f631844e2efec87344193d0796920f419
rafalalala <rafel.ay23@gmail.com>
2025-09-11 20:48:33 +0300

    add if if teacher exist or not and add salary if teacher exist

--- database/crud.py
+++ database/crud.py
@@ -9,37 +9,46 @@ def select_teacher():
   with Session (engine) as session:
      statement = select(Teacher)
      result = session.exec(statement)
-     for teach_all in result:
-        print (teach_all)
-
+     for teach in result:
+        print(f"ID: {teach.id}, Name teacher : {teach.name}, Salary = {teach.salary}")
 
 
 def add_newteacher():
-  teacher5 =Teacher(name = " Dr. Aseel Aesar", salary = 40000)
-  with Session (engine) as session:
-     session.add(teacher5)
-     session.commit()
-     session.refresh(teacher5)
+    with Session (engine) as session:
+       statement = select(Teacher). where (Teacher.name == "Dr. Aseel Aesar")
+       teachfound = session.exec(statement).first()
+       if teachfound:
+          print("the teacher 'Dr. Aseel Aesar' already exists")
+       else:
+          
+          teacher5 =Teacher(name = " Dr. Aseel Aesar", salary = 40000)
+          session.add(teacher5)
+          session.commit()
+          session.refresh(teacher5)
 
      
 def update_money():
   with Session (engine) as session:
    statement = select( Teacher ).where (Teacher.name == "Dr. Sarah Johnson")
-   result = session.exec(statement)
-  #  teacher = result.one() 
+   teacher = session.exec(statement).first()
 
+
+   if teacher :
+      print("the teacher 'Dr. Sarah Johnson' exists")
   # او استخرم firstفي حال الاسم مايكون موجود او موجود اكثر من مرة
-   teacher = result.first()
-   if teacher:  #يتحقق لذا المعلم موجود او موجود اكثر من مرة واذا ماكو راح يرجع none
+      # if teacher:  #يتحقق لذا المعلم موجود او موجود اكثر من مرة واذا ماكو راح يرجع none
     
-    teacher.salary = 60000
-    session.commit()
+      teacher.salary = 60000
+      session.commit()
+   else:
+      print("the teacher not exist")
 
 
 
 def main ():
-   add_newteacher()
-   update_money()
+
+   # add_newteacher()
+   # update_money()
    select_teacher()
 
 if __name__ == "__main__":
