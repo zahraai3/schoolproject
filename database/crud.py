1000e2b5a84e027915f577a085b557fb8bcd8860
rafalalala <rafel.ay23@gmail.com>
2025-09-11 18:27:37 +0300

    added a new teacher and updated a teachar salary and queried all teachers than imported models and engine from other file

---
+++ database/crud.py
@@ -0,0 +1,46 @@
+
+
+from sqlmodel import Session, select
+from database.models import Teacher
+from database.dbctrl import engine
+
+
+def select_teacher():
+  with Session (engine) as session:
+     statement = select(Teacher)
+     result = session.exec(statement)
+     for teach_all in result:
+        print (teach_all)
+
+
+
+def add_newteacher():
+  teacher5 =Teacher(name = " Dr. Aseel Aesar", salary = "400000")
+  with Session (engine) as session:
+     session.add(teacher5)
+     session.commit()
+     session.refresh(teacher5)
+
+     
+def update_money():
+  with Session (engine) as session:
+   statement = select( Teacher ).where (Teacher.name == "Dr. Sarah Johnson")
+   result = session.exec(statement)
+  #  teacher = result.one() 
+
+  # او استخرم firstفي حال الاسم مايكون موجود او موجود اكثر من مرة
+   teacher = result.first()
+   if teacher:  #يتحقق لذا المعلم موجود او موجود اكثر من مرة واذا ماكو راح يرجع none
+    
+    teacher.salary = "600000 IDQ"
+    session.commit()
+
+
+
+def main ():
+   add_newteacher()
+   update_money()
+#    select_teacher()
+
+if __name__ == "__main__":
+     main()
